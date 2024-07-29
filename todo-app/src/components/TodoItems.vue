f4dfc32c24c6463b6e186ed0588b746e530869cb
kaiostavares <kaiosandek@outlook.com>
2024-07-29 18:37:19 -0300

    feat(todo-app): create components for the Todo and add pinia to store data of todos

---
+++ todo-app/src/components/TodoItems.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="space-y-2">
+    <TodoItem
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+    />
+  </div>
+</template>
+
+
+<script>
+import TodoItem from '@/components/TodoItem.vue';
+import { useTodosStore } from '@/store';
+export default {
+  components: { TodoItem },
+  computed:{
+    todos(){
+      return useTodosStore().todos;
+    }
+  }
+}
+</script>
